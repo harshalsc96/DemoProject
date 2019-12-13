25feb84afb7f318cb24cddc0b44deedca373f6f8
Harshal.Chaudhari <Harshal.Chaudhari@ASIA29134>
2019-12-13 12:09:40 +0530

    Updates

---
+++ precache-manifest.477038bb6995e3e78b06bc4003811ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28de6fe4a1f7c7cfe4b80855dc739d40",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "751a0ef4fcd390287a88",
+    "url": "/myapp/static/css/main.be0fe2cf.chunk.css"
+  },
+  {
+    "revision": "b5bad67dc8e8f6f50c93",
+    "url": "/myapp/static/js/2.4c1bbe77.chunk.js"
+  },
+  {
+    "revision": "751a0ef4fcd390287a88",
+    "url": "/myapp/static/js/main.f2435f0f.chunk.js"
+  },
+  {
+    "revision": "a78cc5b79a30139f8c48",
+    "url": "/myapp/static/js/runtime-main.18fcdf32.js"
+  }
+]);
